c69f4be48b4a03f6e9e9ba62470172fb53594905
luost26 <luo_shitong@163.com>
2019-03-19 02:07:07 +0800

    checkpoint

--- WrappedGLFW/GL.hpp
+++ WrappedGLFW/GL.hpp
@@ -8,14 +8,37 @@
 #define GL_hpp
 
 #include "support.hpp"
+#include "Program.hpp"
+#include <glm/vec4.hpp>
 
 namespace wglfw {
     
+    class Cleaner {
+    public:
+        virtual void clean();
+    };
+    
+    class ColorCleaner : public Cleaner {
+    public:
+        glm::vec4 rgba;
+        ColorCleaner(glm::vec4 color):rgba(color) {};
+        void clean() {
+            glClearColor(rgba.r, rgba.g, rgba.b, rgba.a);
+            glClear(GL_COLOR_BUFFER_BIT);
+        }
+    };
+    
+    
     class GL {
     public:
-        static void unbindVertexArray() {
-            glBindVertexArray(0);
+        static void useProgram(Program * prog) {
+            glUseProgram(prog->program());
         }
+        
+        static void clear(Cleaner * cleaner) {
+            cleaner->clean();
+        }
+        
     };
     
 }
