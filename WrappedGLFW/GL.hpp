a444de3759f818651a1c51950e48b0964c724dda
luost26 <luo_shitong@163.com>
2019-03-20 09:03:10 +0800

    Update GL.hpp

--- WrappedGLFW/GL.hpp
+++ WrappedGLFW/GL.hpp
@@ -10,24 +10,62 @@
 #include "support.hpp"
 #include "Program.hpp"
 #include <glm/glm.hpp>
+#include <vector>
 
 namespace wglfw {
     
     class Cleaner {
     public:
-        virtual void clean() {}
-    };
-    
-    class ColorCleaner : public Cleaner {
-    public:
-        glm::vec4 rgba;
-        ColorCleaner(glm::vec4 color):rgba(color) {};
-        void clean() {
-            glClearColor(rgba.r, rgba.g, rgba.b, rgba.a);
-            glClear(GL_COLOR_BUFFER_BIT);
+        virtual void prepare() {}
+        virtual unsigned int option() {
+            return 0;
         }
     };
     
+    class ColorBufferCleaner : public Cleaner {
+    public:
+        glm::vec4 rgba;
+        ColorBufferCleaner(glm::vec4 color):rgba(color) {};
+        void prepare() {
+            glClearColor(rgba.r, rgba.g, rgba.b, rgba.a);
+        }
+        
+        unsigned int option() {
+            return GL_COLOR_BUFFER_BIT;
+        }
+    };
+    
+    class DepthBufferCleaner : public Cleaner {
+    public:
+        unsigned int option() {
+            return GL_DEPTH_BUFFER_BIT;
+        }
+    };
+    
+    
+    class CleanerCollection {
+    private:
+        std::vector<Cleaner *> collection;
+    public:
+        CleanerCollection * add(Cleaner * cleaner) {
+            collection.push_back(cleaner);
+            return this;
+        }
+        
+        void prepare() {
+            for (auto x : collection) {
+                x->prepare();
+            }
+        }
+        
+        unsigned int option() {
+            unsigned int merged = 0;
+            for (auto x : collection) {
+                merged |= x->option();
+            }
+            return merged;
+        }
+    };
     
     class GL {
     public:
@@ -35,8 +73,9 @@ namespace wglfw {
             glUseProgram(prog->program());
         }
         
-        static void clear(Cleaner * cleaner) {
-            cleaner->clean();
+        static void clear(CleanerCollection * c) {
+            c->prepare();
+            glClear(c->option());
         }
         
         static void setViewport(GLint x, GLint y, GLsizei width, GLsizei height) {
@@ -47,8 +86,20 @@ namespace wglfw {
             glDrawElements(mode, count, type, indices);
         }
         
+        static void drawArrays(GLenum mode, GLint first, GLsizei count) {
+            glDrawArrays(mode, first, count);
+        }
+        
         static void setPolygonMode(GLenum face, GLenum mode) {
             glPolygonMode(face, mode);
+        }
+        
+        static void enable(GLenum cap) {
+            glEnable(cap);
+        }
+        
+        static void enableDepthTest() {
+            glEnable(GL_DEPTH_TEST);
         }
     };
     
