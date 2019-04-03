7e0e6f4911dc5ed6f330a632bba2c4cfd0381719
luost26 <luo_shitong@163.com>
2019-04-03 16:34:46 +0800

    rename WrappedGLFW loader header file

--- WrappedGLFW/loader.hpp
+++ WrappedGLFW/loader.hpp
@@ -7,22 +7,6 @@
 #ifndef loader_hpp
 #define loader_hpp
 
-/******    Core    ******/
-#include "GLFW.hpp"
-#include "Window.hpp"
-#include "GL.hpp"
-#include "Shader.hpp"
-#include "Program.hpp"
-#include "VertexArray.hpp"
-#include "ArrayBuffer.hpp"
-#include "Texture.hpp"
-#include "FrameBuffer.hpp"
-#include "RenderBuffer.hpp"
-
-/****** Utilities ******/
-#include "Utilities/helper.hpp"
-#include "Utilities/Camera.hpp"
-#include "Utilities/Mesh.hpp"
-#include "Utilities/AssimpModel.hpp"
+#include "wglfw.hpp"
 
 #endif /* loader_hpp */
---
+++ WrappedGLFW/wglfw.hpp
@@ -0,0 +1,28 @@
+//
+//  wglfw.hpp
+//
+//  Created by 罗 世通 on 2019/4/3.
+//
+
+#ifndef wglfw_hpp
+#define wglfw_hpp
+
+/******    Core    ******/
+#include "GLFW.hpp"
+#include "Window.hpp"
+#include "GL.hpp"
+#include "Shader.hpp"
+#include "Program.hpp"
+#include "VertexArray.hpp"
+#include "ArrayBuffer.hpp"
+#include "Texture.hpp"
+#include "FrameBuffer.hpp"
+#include "RenderBuffer.hpp"
+
+/****** Utilities ******/
+#include "Utilities/helper.hpp"
+#include "Utilities/Camera.hpp"
+#include "Utilities/Mesh.hpp"
+#include "Utilities/AssimpModel.hpp"
+
+#endif /* wglfw_hpp */
