e014c4e044c32b4a9652a31a8529e8e87ad3546b
luost26 <luo_shitong@163.com>
2019-04-06 01:21:34 +0800

    integrate perspective transformation to 'Camera'

--- WrappedGLFW/Utilities/Camera.hpp
+++ WrappedGLFW/Utilities/Camera.hpp
@@ -17,7 +17,11 @@ namespace wglfw {
     private:
         glm::vec3 _position, _worldUp;
         glm::vec3 _up, _front, _right;
+        
         float _yaw, _pitch;
+        float _fieldOfView, _fovNear, _fovFar;
+        unsigned int _viewportWidth, _viewportHeight;
+        glm::mat4 _projection;
         
         void updateCameraVectors() {
             glm::vec3 front;
@@ -42,38 +46,78 @@ namespace wglfw {
             }
         }
         
+        void updateProjectionMatrix() {
+            _projection = glm::perspective(fieldOfViewRadians(), viewportAspectRatio(), _fovNear, _fovFar);
+        }
+        
     public:
-        Camera(const glm::vec3 & eye, const glm::vec3 & center, const glm::vec3 & top, const glm::vec3 & worldtop) {
+        Camera(const glm::vec3 & eye, const glm::vec3 & center, const glm::vec3 & top, const glm::vec3 & worldtop, unsigned int & width, unsigned int & height, const float & fov=45.0, const float & fov_near=0.1, const float & fov_far=100.0) {
             _position = eye;
             _front = glm::normalize(center - eye);
             _up = glm::normalize(top);
             _worldUp = glm::normalize(worldtop);
+            _viewportWidth = width;
+            _viewportHeight = height;
+            _fieldOfView = fov;
+            _fovNear = fov_near;
+            _fovFar = fov_far;
             updateYawPitch();
             updateCameraVectors();
+            updateProjectionMatrix();
         }
         
-        Camera(const glm::vec3 & eye, const float yaw, const float pitch, const glm::vec3 & worldtop) {
+        Camera(const glm::vec3 & eye, const float yaw, const float pitch, const glm::vec3 & worldtop, unsigned int & width, unsigned int & height, const float & fov=45.0, const float & fov_near=0.1, const float & fov_far=100.0) {
             _position = eye;
             _worldUp = worldtop;
+            _viewportWidth = width;
+            _viewportHeight = height;
+            _fieldOfView = fov;
+            _fovNear = fov_near;
+            _fovFar = fov_far;
             updateCameraVectors();
+            updateProjectionMatrix();
         }
         
-        Camera(const glm::vec3 & eye, const glm::vec3 & center, const glm::vec3 & top): Camera(eye, center, top, top) {}
+        Camera(const glm::vec3 & eye, const glm::vec3 & center, const glm::vec3 & top, unsigned int & width, unsigned int & height, const float & fov=45.0, const float & fov_near=0.1, const float & fov_far=100.0): Camera(eye, center, top, top, width, height, fov, fov_near, fov_far) {}
         
         inline const glm::mat4 viewMatrix() const {
             return glm::lookAt(_position, _position+_front, _up);
+        }
+        
+        inline const glm::mat4 projectionMatrix() const {
+            return _projection;
         }
         
         operator glm::mat4 () const {
             return viewMatrix();
         }
         
-        glm::vec3 position() {
+        inline const glm::vec3 position() const {
             return _position;
         }
         
-        glm::vec3 front() {
+        inline const glm::vec3 front() const {
             return _front;
+        }
+        
+        inline const float fieldOfViewDegrees() const {
+            return _fieldOfView;
+        }
+        
+        inline const float fieldOfViewRadians() const {
+            return glm::radians(_fieldOfView);
+        }
+        
+        inline const unsigned int viewportWidth() const {
+            return _viewportWidth;
+        }
+        
+        inline const unsigned int viewportHeight() const {
+            return _viewportHeight;
+        }
+        
+        inline const float viewportAspectRatio() const {
+            return (float)_viewportWidth / (float)_viewportHeight;
         }
         
         inline void moveUp(const float & speed) {
@@ -118,6 +162,36 @@ namespace wglfw {
         inline void lookLeft(const float & ddeg) {
             _yaw -= ddeg;
             updateCameraVectors();
+        }
+        
+        inline void zoomIn(const float & ddeg) {
+            _fieldOfView -= ddeg;
+            updateProjectionMatrix();
+        }
+        
+        inline void zoomOut(const float & ddeg) {
+            _fieldOfView += ddeg;
+            updateProjectionMatrix();
+        }
+        
+        inline void setViewportWidth(unsigned int s) {
+            _viewportWidth = s;
+            updateProjectionMatrix();
+        }
+        
+        inline void setViewportHeight(unsigned int s) {
+            _viewportHeight = s;
+            updateProjectionMatrix();
+        }
+        
+        inline void setFoVNear(const float & d) {
+            _fovNear = d;
+            updateProjectionMatrix();
+        }
+        
+        inline void setFoVFar(const float & d) {
+            _fovFar = d;
+            updateProjectionMatrix();
         }
         
     };
